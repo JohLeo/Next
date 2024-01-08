36c4746ff485fc27ba747cd7a4172ba54d07cda2
Johanna Leonsson <johanna@leonsson.se>
2024-01-08 16:26:54 +0100

    setting up links

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,6 +1,16 @@
 
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div>Home Page</div>
+    <div>
+      <div>
+        <Link href="/performance"> Performance </Link>
+        <Link href="/reliability"> Reliability </Link>
+        <Link href="/scale"> Scale </Link>
+      </div>
+      Home Page
+
+    </div>
   )
 }
