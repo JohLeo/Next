80f48961057f57752030fc28de8a4eb6cc0d3258
Johanna Leonsson <johanna@leonsson.se>
2024-01-09 16:01:54 +0100

    adding cover image

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,12 +1,19 @@
-
-import Link from 'next/link';
+import Image from 'next/image';
+import homeImg from '/public/home.jpg';
 
 export default function Home() {
   return (
     <div>
 
       Home Page
-
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homeImg}
+          alt="factory of sort"
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
     </div>
   )
 }
