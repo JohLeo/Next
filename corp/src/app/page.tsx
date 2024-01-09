7f038aec30604e202635c3a33e88cda696588744
Johanna Leonsson <johanna@leonsson.se>
2024-01-09 16:22:21 +0100

    adding reusable presentation comp and some styling

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,19 +1,12 @@
-import Image from 'next/image';
 import homeImg from '/public/home.jpg';
+import Hero from '@/components/hero';
 
 export default function Home() {
   return (
-    <div>
-
-      Home Page
-      <div className="absolute -z-10 inset-0">
-        <Image
-          src={homeImg}
-          alt="factory of sort"
-          fill
-          style={{ objectFit: 'cover' }}
-        />
-      </div>
-    </div>
-  )
+    <Hero
+      imgData={homeImg}
+      imgAlt="factory"
+      title="Pro Cloud Hosting"
+    />
+  );
 }
