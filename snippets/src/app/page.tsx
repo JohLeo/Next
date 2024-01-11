51d1600d10af13ca7b17f344566af272a888c8f4
Johanna Leonsson <johanna@leonsson.se>
2024-01-11 17:15:50 +0100

    fetching data with server component

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,7 +1,15 @@
+import { db } from "@/db"
 
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
 
-export default function Home() {
-  return (
-    <div>Home Page</div>
-  )
+  const renderedSnippets = snippets.map((snippet) => {
+    return (
+      <div key={snippet.id}>
+        {snippet.title}
+      </div>
+    )
+  });
+
+  return <div>{renderedSnippets}</div>
 }
