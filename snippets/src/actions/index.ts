f6ecfdd076c445ac7b050b2ca8b1a551954e8061
Johanna Leonsson <johanna@leonsson.se>
2024-01-16 16:45:43 +0100

    adding useformstate hook

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -20,3 +20,28 @@ export async function deleteSnippet(id: number) {
 
   redirect('/');
 };
+
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  return {
+    message: 'Title must be longer'
+  };
+
+  // check the user's inputs and make sure they're valid (from inputs. as sting used to simplify it for ts)
+  // const title = formData.get('title') as string;
+  // const code = formData.get('code') as string;
+
+  // create a new record in the database
+  // const snippet = await db.snippet.create({
+  //   data: {
+  //     title,
+  //     code,
+  //   },
+  // });
+
+  // Redirect the user back to the root route
+  // redirect('/');
+}
