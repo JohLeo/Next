363c07ee166f81f9342c3d119b23338604f5c012
Johanna Leonsson <johanna@leonsson.se>
2024-01-16 17:08:53 +0100

    catch setup for err message

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -26,29 +26,42 @@ export async function createSnippet(
   formState: { message: string },
   formData: FormData
 ) {
-  // check the user's inputs and make sure they're valid (from inputs. as sting used to simplify it for ts)
-  const title = formData.get('title');
-  const code = formData.get('code');
+  try {
+    // check the user's inputs and make sure they're valid (from inputs. as sting used to simplify it for ts)
+    const title = formData.get('title');
+    const code = formData.get('code');
 
-  if (typeof title !== 'string' || title.length < 3) {
-    return {
-      message: 'Title must be longer',
-    };
-  }
-  if (typeof code !== 'string' || code.length < 10) {
-    return {
-      message: 'Code must be longer',
-    };
+    if (typeof title !== 'string' || title.length < 3) {
+      return {
+        message: 'Title must be longer',
+      };
+    }
+    if (typeof code !== 'string' || code.length < 10) {
+      return {
+        message: 'Code must be longer',
+      };
+    }
+
+    // create a new record in the database
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: 'Something went wrong...',
+      };
+    }
   }
 
-  // create a new record in the database
-  // const snippet = await db.snippet.create({
-  //   data: {
-  //     title,
-  //     code,
-  //   },
-  // });
-  throw new Error('Duude thats an oops!!!');
 
   // Redirect the user back to the root route
   redirect('/');
