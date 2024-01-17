06f790204fc3022c97cec42531a9e390202b96c7
Johanna Leonsson <johanna@leonsson.se>
2024-01-17 16:08:05 +0100

    live and learn

--- pinkit/src/app/page.tsx
+++ pinkit/src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from '@nextui-org/react';
 export default function Home() {
   return (
     <div>
-      <Button>Click mee!</Button>
+      <Button>Click me!</Button>
     </div>
   )
 }
