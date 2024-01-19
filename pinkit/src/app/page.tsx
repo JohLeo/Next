c585ad25493757143625845f93e6c40d6bbbd1a8
Johanna Leonsson <johanna@leonsson.se>
2024-01-19 14:53:10 +0100

    in and out and checking auth status

--- pinkit/src/app/page.tsx
+++ pinkit/src/app/page.tsx
@@ -2,6 +2,7 @@
 import { Button } from '@nextui-org/react';
 import * as actions from '@/actions';
 import { auth } from '@/auth';
+import Profile from '@/components/profile';
 
 export default async function Home() {
   const session = await auth();
@@ -18,9 +19,14 @@ export default async function Home() {
         <Button type="submit">Sign out</Button>
       </form>
 
-      {
-        session?.user ? <div>{JSON.stringify(session.user)}</div> : <div> Signed out </div>
-      }
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div> Signed out </div>
+      )}
+
+      <Profile />
+
     </div>
   )
 }
