631921a4479a998df17a98c0781eb90c8cac7fc5
Johanna Leonsson <johanna@leonsson.se>
2024-01-22 14:04:19 +0100

    setting up actions for authenticated login and out plus some styling

--- pinkit/src/components/header.tsx
+++ pinkit/src/components/header.tsx
@@ -9,9 +9,33 @@ import {
   Avatar
 } from '@nextui-org/react';
 import { auth } from '@/auth';
+import * as actions from '@/actions';
 
 export default async function Header() {
   const session = await auth();
+
+  let authContent: React.ReactNode;
+  if (session?.user) {
+    authContent = <Avatar src={session.user.image || ''} />
+  } else {
+    authContent = <>
+      <NavbarItem>
+        <form action={actions.signIn}>
+          <Button type="submit" color="secondary" variant="bordered">
+            Sign In
+          </Button>
+        </form>
+      </NavbarItem>
+
+      <NavbarItem>
+        <form action={actions.signIn}>
+          <Button type="submit" color="primary" variant="flat">
+            Sign Up
+          </Button>
+        </form>
+      </NavbarItem>
+    </>
+  }
 
   return (
     <Navbar className="shadow mb-6">
@@ -26,11 +50,7 @@ export default async function Header() {
       </NavbarContent>
 
       <NavbarContent justify="end">
-        <NavbarItem>
-          {
-            session?.user ? <div>Signed in</div> : <div>Signed out</div>
-          }
-        </NavbarItem>
+        {authContent}
       </NavbarContent>
 
     </Navbar>
