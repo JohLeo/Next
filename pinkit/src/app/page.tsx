3bab019e329a6ec41d10b37fa99301345a9109e6
Johanna Leonsson <johanna@leonsson.se>
2024-01-22 14:51:19 +0100

    setting up topic structure and form, implementing form action

--- pinkit/src/app/page.tsx
+++ pinkit/src/app/page.tsx
@@ -1,9 +1,17 @@
-
+import TopicCreateForm from '@/components/topics/topic-create-form';
 
 export default async function Home() {
 
   return (
-
-    <div>Home Page</div>
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m-2">
+          Top posts
+        </h1>
+      </div>
+      <div>
+        <TopicCreateForm />
+      </div>
+    </div>
   )
 }
