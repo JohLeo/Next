2928f8ad6d33f764bfd61e7164fd2ac36dac8595
Johanna Leonsson <johanna@leonsson.se>
2024-01-23 16:17:43 +0100

    handling general errors

--- pinkit/src/components/topics/topic-create-form.tsx
+++ pinkit/src/components/topics/topic-create-form.tsx
@@ -47,6 +47,12 @@ export default function TopicCreateForm() {
               errorMessage={formState.errors.description?.join(', ')}
             />
 
+            {formState.errors._form ? (
+              <div className="rounded p-2 bg-red-200 border border-red-400">
+                {formState.errors._form?.join(', ')}
+              </div>
+            ) : null}
+
             <Button type="submit">
               Submit
             </Button>
