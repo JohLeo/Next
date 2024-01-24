2afca436545d52ee19b0f62c3f6ae3fa641da7fd
Johanna Leonsson <johanna@leonsson.se>
2024-01-24 14:24:17 +0100

    forgo the form action...

--- pinkit/src/components/posts/post-create-form.tsx
+++ pinkit/src/components/posts/post-create-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useFormState } from 'react-dom';
 import {
   Input,
@@ -11,6 +13,12 @@ import * as actions from '@/actions';
 import FormButton from '@/components/common/form-button';
 
 export default function PostCreateForm() {
+  const [formState, action] = useFormState(
+    actions.createPost,
+    {
+      errors: {}
+    }
+  );
 
   return (
     <Popover placement="left">
@@ -20,20 +28,24 @@ export default function PostCreateForm() {
         </Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg>">
               Create a post
             </h3>
 
             <Input
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(', ')}
               name="title"
               label="Title"
               labelPlacement="outside"
               placeholder="Title"
             />
 
-            <Input
+            <Textarea
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(', ')}
               name="content"
               label="Content"
               labelPlacement="outside"
